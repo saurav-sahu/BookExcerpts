41bf07cbaa12a12038de31c5d0ed1cbf25180254
Rover8 <sauravks@usc.edu>
2021-10-18 03:52:36 +0000

    Replaced with Typescript.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      content coming here soon!
+    </div>
+  );
+}
+
+export default App;
